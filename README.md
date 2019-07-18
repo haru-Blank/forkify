00d23e6fe3870172651e9b98ce9243b4958313e6
avashNekros <avash.hunted247@gmail.com>
2019-07-18 07:06:15 +0545

    Forkify

---
+++ README.md
@@ -0,0 +1,5 @@
+ 
+
+# Forkify
+
+Search for recipies of thousand foods.
---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const key = '7c28beccb5ef9c465976e3ee3c8cf77f';
